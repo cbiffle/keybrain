fde8a99fdf0f334ae7a098dc8fb8def63c18c44e
Cliff L. Biffle <code@cliffle.com>
2020-08-09 16:46:33 -0700

    fourk/fw: continue factoring out driver

--- fw/src/main.rs
+++ fw/src/main.rs
@@ -246,35 +246,6 @@ fn setup_usb_buffers(
     usb.btable.write(|w| unsafe { w.bits(btable_off as u32) });
 }
 
-fn setup_usb(
-    usb: &device::USB,
-) {
-    usb.istr.write(|w| unsafe {
-        //             v--- RESET
-        w.bits(0b0111_1011_1000_0000)
-    });
-
-    // Set up control EP 0 for enumeration.
-    usb.epr[0].modify(|r, w| {
-        w.ea().bits(0)
-            .ep_type().bits(0b01) // CONTROL
-            .ep_kind().clear_bit() // not STATUS_OUT
-
-            // Note: these bits are toggled by writing 1 for some goddamn
-            // reason, so we set them as follows. I'd love to extract a utility
-            // function for this but svd2rust has ensured that this is
-            // impossible.
-            .dtog_tx().bit(r.dtog_tx().bit()) // clear bit by toggle
-            .stat_tx().bits(r.stat_tx().bits() ^ 0b11) // VALID
-
-            .dtog_rx().bit(r.dtog_rx().bit()) // clear bit by toggle
-            .stat_rx().bits(r.stat_rx().bits() ^ 0b11) // VALID (can receive)
-    });
-
-    // Configure to respond to address 0.
-    usb.daddr.write(|w| w.ef().set_bit());
-}
-
 const USB_SRAM_BASE: usize = 0x4000_6C00;
 const USB_SRAM_SIZE: usize = 1024;
 
@@ -828,13 +799,40 @@ enum DeviceState {
 struct Device {
     state: DeviceState,
     pending_address: Option<u8>,
+
+    iface: Hid,
 }
 
 impl Device {
     pub fn reset(&mut self, usb: &device::USB) {
-        setup_usb(usb);
+        usb.istr.write(|w| unsafe {
+            //             v--- RESET
+            w.bits(0b0111_1011_1000_0000)
+        });
+
+        // Set up control EP 0 for enumeration.
+        usb.epr[0].modify(|r, w| {
+            w.ea().bits(0)
+                .ep_type().bits(0b01) // CONTROL
+                .ep_kind().clear_bit() // not STATUS_OUT
+
+                // Note: these bits are toggled by writing 1 for some goddamn
+                // reason, so we set them as follows. I'd love to extract a
+                // utility function for this but svd2rust has ensured that this
+                // is impossible.
+                .dtog_tx().bit(r.dtog_tx().bit()) // clear bit by toggle
+                .stat_tx().bits(r.stat_tx().bits() ^ 0b11) // VALID
+
+                .dtog_rx().bit(r.dtog_rx().bit()) // clear bit by toggle
+                .stat_rx().bits(r.stat_rx().bits() ^ 0b11) // VALID (can receive)
+        });
+
+        // Configure to respond to address 0.
+        usb.daddr.write(|w| w.ef().set_bit());
         self.state = DeviceState::Default;
         self.pending_address = None;
+
+        self.iface = Hid::default();
     }
 
     pub fn on_out(&mut self, ep: usize, usb: &device::USB) {
@@ -891,33 +889,8 @@ impl Device {
                         }
                     }
                     (Dir::HostToDevice, Some(StdRequestCode::SetConfiguration)) => {
-                        // TODO this should be factored out into a configuration
-                        // layer
                         set_ep_tx_count(usb, ep, 0);
-                        // Prepare empty report for EP1.
-                        let txoff = get_ep_tx_offset(usb, 1);
-                        write_usb_sram_16(txoff, 0);
-                        write_usb_sram_16(txoff + 2, 0);
-                        write_usb_sram_16(txoff + 4, 0);
-                        write_usb_sram_16(txoff + 6, 0);
-                        set_ep_tx_count(usb, 1, 8);
-
-                        // Set up EP1 for HID
-                        usb.epr[1].modify(|r, w| {
-                            w.ea().bits(1)
-                                .ep_type().bits(0b11) // INTERRUPT
-                                .ep_kind().clear_bit() // not used
-
-                                // Note: these bits are toggled by writing 1 for some goddamn
-                                // reason, so we set them as follows. I'd love to extract a utility
-                                // function for this but svd2rust has ensured that this is
-                                // impossible.
-                                .dtog_tx().bit(r.dtog_tx().bit()) // clear bit by toggle
-                                .stat_tx().bits(r.stat_tx().bits() ^ 0b11) // VALID
-
-                                .dtog_rx().bit(r.dtog_rx().bit()) // clear bit by toggle
-                                .stat_rx().bits(r.stat_rx().bits() ^ 0b01) // STALL (can't receive)
-                        });
+                        self.iface.on_set_config(usb);
                         configure_response(usb, 0, Status::Valid, Status::Valid);
                         self.state = DeviceState::Configured;
                     }
@@ -929,55 +902,11 @@ impl Device {
                         configure_response(usb, 0, Status::Stall, Status::Stall);
                     }
                 },
-                (RequestTypeType::Standard, Recipient::Interface) => match (setup.request_type.data_phase_direction(), StdRequestCode::from_u8(setup.request)) {
-                    (Dir::DeviceToHost, Some(StdRequestCode::GetDescriptor)) => {
-                        match HidClassDescriptorType::from_u16(setup.value.get() >> 8) {
-                            Some(HidClassDescriptorType::Report) => {
-                                // HID Report Descriptor
-                                let desc = [
-                                    0x05, 0x01, 0x09, 0x06, 0xa1, 0x01, 0x05, 0x07, 0x19, 0xe0, 0x29, 0xe7, 0x15, 0x00, 0x25, 0x01,
-                                    0x75, 0x01, 0x95, 0x08, 0x81, 0x02, 0x95, 0x01, 0x75, 0x08, 0x81, 0x01, 0x95, 0x03, 0x75, 0x01,
-                                    0x05, 0x08, 0x19, 0x01, 0x29, 0x03, 0x91, 0x02, 0x95, 0x05, 0x75, 0x01, 0x91, 0x01, 0x95, 0x06,
-                                    0x75, 0x08, 0x26, 0xff, 0x00, 0x05, 0x07, 0x19, 0x00, 0x29, 0x91, 0x81, 0x00, 0xc0,
-                                ];
-                                write_usb_sram_bytes(get_ep_tx_offset(usb, 0), &desc);
-                                // Update transmittable count.
-                                set_ep_tx_count(usb, ep, setup.length.get().min(desc.len() as u16));
-                            }
-                            _ => {
-                                // Unknown kind of descriptor.
-                                // TODO stall
-                                set_ep_tx_count(usb, ep, 0);
-                            }
-                        }
-                        // Enable transmission.
-                        configure_response(usb, ep, Status::Valid, Status::Valid);
-                    }
-                    _ => {
-                        // Unsupported
-                        // Update transmittable count.
-                        set_ep_tx_count(usb, ep, 0);
-                        // Set a stall condition.
-                        configure_response(usb, ep, Status::Stall, Status::Stall);
-                    }
+                (RequestTypeType::Standard, Recipient::Interface) => {
+                    self.iface.on_setup_iface_std(&setup, usb);
                 }
-                (RequestTypeType::Class, Recipient::Interface) => match (setup.request_type.data_phase_direction(), HidRequestCode::from_u8(setup.request)) {
-                    (Dir::HostToDevice, Some(HidRequestCode::SetIdle)) => {
-                        set_ep_tx_count(usb, ep, 0);
-                        configure_response(usb, ep, Status::Valid, Status::Valid);
-                    }
-                    (Dir::HostToDevice, Some(HidRequestCode::SetReport)) => {
-                        // whatever
-                        set_ep_tx_count(usb, ep, 0);
-                        configure_response(usb, ep, Status::Valid, Status::Valid);
-                    }
-                    _ => {
-                        // Unsupported
-                        // Update transmittable count.
-                        set_ep_tx_count(usb, ep, 0);
-                        // Set a stall condition.
-                        configure_response(usb, ep, Status::Stall, Status::Stall);
-                    }
+                (RequestTypeType::Class, Recipient::Interface) => {
+                    self.iface.on_setup_iface_class(&setup, usb);
                 }
                 _ => {
                     // Unsupported
@@ -1009,29 +938,7 @@ impl Device {
 
             configure_response(usb, ep, Status::Valid, Status::Valid);
         } else {
-            // The host has just read a HID report. Prepare the next one.
-            // TODO this introduces one stage of queueing delay; the reports
-            // should be generated asynchronously.
-
-            // We have a key status matrix. We want a packed list of keycodes.
-            // Scan the matrix to convert.
-            let mut write_idx = 2;
-            let txoff = get_ep_tx_offset(usb, 1);
-            for (dn_row, code_row) in keys_down.iter().zip(&KEYS) {
-                for (dn, code) in dn_row.iter().zip(code_row) {
-                    if *dn {
-                        write_usb_sram_8(txoff + write_idx, *code);
-                        write_idx += 1;
-                    }
-                }
-            }
-            // Pad the rest of the report with zeroes.
-            while write_idx < 8 {
-                write_usb_sram_8(txoff + write_idx, 0);
-                write_idx += 1;
-            }
-
-            configure_response(usb, ep, Status::Valid, Status::Valid);
+            self.iface.on_in(ep, usb, keys_down);
         }
     }
 }
@@ -1049,4 +956,119 @@ fn configure_response(usb: &device::USB, ep: usize, tx: Status, rx: Status) {
             .stat_tx().bits(r.stat_tx().bits() ^ tx as u8)
             .stat_rx().bits(r.stat_rx().bits() ^ rx as u8)
     });
+}
+
+#[derive(Debug, Default, Clone)]
+struct Hid {
+}
+
+impl Hid {
+    fn on_set_config(&mut self, usb: &device::USB) {
+        // Prepare empty report for EP1.
+        let txoff = get_ep_tx_offset(usb, 1);
+        write_usb_sram_16(txoff, 0);
+        write_usb_sram_16(txoff + 2, 0);
+        write_usb_sram_16(txoff + 4, 0);
+        write_usb_sram_16(txoff + 6, 0);
+        set_ep_tx_count(usb, 1, 8);
+
+        // Set up EP1 for HID
+        usb.epr[1].modify(|r, w| {
+            w.ea().bits(1)
+                .ep_type().bits(0b11) // INTERRUPT
+                .ep_kind().clear_bit() // not used
+
+                // Note: these bits are toggled by writing 1 for some goddamn
+                // reason, so we set them as follows. I'd love to extract a utility
+                // function for this but svd2rust has ensured that this is
+                // impossible.
+                .dtog_tx().bit(r.dtog_tx().bit()) // clear bit by toggle
+                .stat_tx().bits(r.stat_tx().bits() ^ 0b11) // VALID
+
+                .dtog_rx().bit(r.dtog_rx().bit()) // clear bit by toggle
+                .stat_rx().bits(r.stat_rx().bits() ^ 0b01) // STALL (can't receive)
+        });
+    }
+
+    fn on_setup_iface_std(&mut self, setup: &SetupPacket, usb: &device::USB) {
+        match (setup.request_type.data_phase_direction(), StdRequestCode::from_u8(setup.request)) {
+            (Dir::DeviceToHost, Some(StdRequestCode::GetDescriptor)) => {
+                match HidClassDescriptorType::from_u16(setup.value.get() >> 8) {
+                    Some(HidClassDescriptorType::Report) => {
+                        // HID Report Descriptor
+                        let desc = [
+                            0x05, 0x01, 0x09, 0x06, 0xa1, 0x01, 0x05, 0x07, 0x19, 0xe0, 0x29, 0xe7, 0x15, 0x00, 0x25, 0x01,
+                            0x75, 0x01, 0x95, 0x08, 0x81, 0x02, 0x95, 0x01, 0x75, 0x08, 0x81, 0x01, 0x95, 0x03, 0x75, 0x01,
+                            0x05, 0x08, 0x19, 0x01, 0x29, 0x03, 0x91, 0x02, 0x95, 0x05, 0x75, 0x01, 0x91, 0x01, 0x95, 0x06,
+                            0x75, 0x08, 0x26, 0xff, 0x00, 0x05, 0x07, 0x19, 0x00, 0x29, 0x91, 0x81, 0x00, 0xc0,
+                        ];
+                        write_usb_sram_bytes(get_ep_tx_offset(usb, 0), &desc);
+                        // Update transmittable count.
+                        set_ep_tx_count(usb, 0, setup.length.get().min(desc.len() as u16));
+                    }
+                    _ => {
+                        // Unknown kind of descriptor.
+                        // TODO stall
+                        set_ep_tx_count(usb, 0, 0);
+                    }
+                }
+                // Enable transmission.
+                configure_response(usb, 0, Status::Valid, Status::Valid);
+            }
+            _ => {
+                // Unsupported
+                // Update transmittable count.
+                set_ep_tx_count(usb, 0, 0);
+                // Set a stall condition.
+                configure_response(usb, 0, Status::Stall, Status::Stall);
+            }
+        }
+    }
+
+    fn on_setup_iface_class(&mut self, setup: &SetupPacket, usb: &device::USB) {
+        match (setup.request_type.data_phase_direction(), HidRequestCode::from_u8(setup.request)) {
+            (Dir::HostToDevice, Some(HidRequestCode::SetIdle)) => {
+                set_ep_tx_count(usb, 0, 0);
+                configure_response(usb, 0, Status::Valid, Status::Valid);
+            }
+            (Dir::HostToDevice, Some(HidRequestCode::SetReport)) => {
+                // whatever
+                set_ep_tx_count(usb, 0, 0);
+                configure_response(usb, 0, Status::Valid, Status::Valid);
+            }
+            _ => {
+                // Unsupported
+                // Update transmittable count.
+                set_ep_tx_count(usb, 0, 0);
+                // Set a stall condition.
+                configure_response(usb, 0, Status::Stall, Status::Stall);
+            }
+        }
+    }
+
+    fn on_in(&mut self, ep: usize, usb: &device::USB, keys_down: &[[bool; 2]; 2]) {
+        // The host has just read a HID report. Prepare the next one.
+        // TODO this introduces one stage of queueing delay; the reports
+        // should be generated asynchronously.
+
+        // We have a key status matrix. We want a packed list of keycodes.
+        // Scan the matrix to convert.
+        let mut write_idx = 2;
+        let txoff = get_ep_tx_offset(usb, 1);
+        for (dn_row, code_row) in keys_down.iter().zip(&KEYS) {
+            for (dn, code) in dn_row.iter().zip(code_row) {
+                if *dn {
+                    write_usb_sram_8(txoff + write_idx, *code);
+                    write_idx += 1;
+                }
+            }
+        }
+        // Pad the rest of the report with zeroes.
+        while write_idx < 8 {
+            write_usb_sram_8(txoff + write_idx, 0);
+            write_idx += 1;
+        }
+
+        configure_response(usb, ep, Status::Valid, Status::Valid);
+    }
 }
