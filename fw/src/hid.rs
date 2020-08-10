b66f934d8a02374946d3caf42ec10870c094d4b4
Cliff L. Biffle <code@cliffle.com>
2020-08-10 08:28:05 -0700

    fourk/fw: document boot keyboard descriptor

--- fw/src/hid.rs
+++ fw/src/hid.rs
@@ -41,6 +41,40 @@ pub struct HidDescriptor {
 pub struct Hid {
 }
 
+static BOOT_KBD_DESC: [u8; 62] = [
+    0x05, 0x01,       //  Usage Page (Desktop),
+    0x09, 0x06,       //  Usage (Keyboard),
+    0xA1, 0x01,       //  Collection (Application),
+    0x05, 0x07,       //      Usage Page (Keyboard),
+    0x19, 0xE0,       //      Usage Minimum (KB Leftcontrol),
+    0x29, 0xE7,       //      Usage Maximum (KB Right GUI),
+    0x15, 0x00,       //      Logical Minimum (0),
+    0x25, 0x01,       //      Logical Maximum (1),
+    0x75, 0x01,       //      Report Size (1),
+    0x95, 0x08,       //      Report Count (8),
+    0x81, 0x02,       //      Input (Variable),
+    0x95, 0x01,       //      Report Count (1),
+    0x75, 0x08,       //      Report Size (8),
+    0x81, 0x01,       //      Input (Constant),
+    0x95, 0x03,       //      Report Count (3),
+    0x75, 0x01,       //      Report Size (1),
+    0x05, 0x08,       //      Usage Page (LED),
+    0x19, 0x01,       //      Usage Minimum (01h),
+    0x29, 0x03,       //      Usage Maximum (03h),
+    0x91, 0x02,       //      Output (Variable),
+    0x95, 0x05,       //      Report Count (5),
+    0x75, 0x01,       //      Report Size (1),
+    0x91, 0x01,       //      Output (Constant),
+    0x95, 0x06,       //      Report Count (6),
+    0x75, 0x08,       //      Report Size (8),
+    0x26, 0xFF, 0x00, //      Logical Maximum (255),
+    0x05, 0x07,       //      Usage Page (Keyboard),
+    0x19, 0x00,       //      Usage Minimum (None),
+    0x29, 0x91,       //      Usage Maximum (KB LANG2),
+    0x81, 0x00,       //      Input,
+    0xC0              //  End Collection
+];
+
 impl Hid {
     pub fn on_set_config(&mut self, usb: &device::USB) {
         // Prepare empty report for EP1.
@@ -75,13 +109,8 @@ impl Hid {
                 match HidClassDescriptorType::from_u16(setup.value.get() >> 8) {
                     Some(HidClassDescriptorType::Report) => {
                         // HID Report Descriptor
-                        let desc = [
-                            0x05, 0x01, 0x09, 0x06, 0xa1, 0x01, 0x05, 0x07, 0x19, 0xe0, 0x29, 0xe7, 0x15, 0x00, 0x25, 0x01,
-                            0x75, 0x01, 0x95, 0x08, 0x81, 0x02, 0x95, 0x01, 0x75, 0x08, 0x81, 0x01, 0x95, 0x03, 0x75, 0x01,
-                            0x05, 0x08, 0x19, 0x01, 0x29, 0x03, 0x91, 0x02, 0x95, 0x05, 0x75, 0x01, 0x91, 0x01, 0x95, 0x06,
-                            0x75, 0x08, 0x26, 0xff, 0x00, 0x05, 0x07, 0x19, 0x00, 0x29, 0x91, 0x81, 0x00, 0xc0,
-                        ];
-                        write_usb_sram_bytes(get_ep_tx_offset(usb, 0), &desc);
+                        let desc = &BOOT_KBD_DESC;
+                        write_usb_sram_bytes(get_ep_tx_offset(usb, 0), desc);
                         // Update transmittable count.
                         set_ep_tx_count(usb, 0, setup.length.get().min(desc.len() as u16));
                     }
